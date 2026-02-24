b90ee9d14014c88c551b63850d6e065f4771d463
jkrts <jkroberts2099@gmail.com>
2026-02-24 13:12:18 -0600

    Update Validation Checks for basic validation of FEN format

--- src/Chess.Core/FenValidator.cs
+++ src/Chess.Core/FenValidator.cs
@@ -11,33 +11,48 @@ public static class FenValidator
         'P', 'N', 'B', 'R', 'Q', 'K',
         '/',
         ' ',
-        '-'
+        '-',
+        '9'
     */
-    private static readonly string ValidFenCharacters = "012345678pnbrqkPNBRQK/-w ";
+
+    // TODO
+    // Break validation down into each part
+    // 1. Move List
+    // 2. Active Color
+    // 3. Castling Availability
+    // 4. En Passant Target
+    // 5. HalfMove clock
+    // 6. Fullmove number
+    // 7. Chess Logic for valid position????
+
+
+    private static readonly string _validFenCharacters = "0123456789pnbrqkPNBRQK/-w ";
+    private static readonly Regex _consecutiveSpaces = new Regex(@"\s{2,}");
 
     public static FenValidationResult Validate(string fen)
     {
 
-        if (!HasOnlyValidFenCharacters(fen))
+        if (string.IsNullOrWhiteSpace(fen))
+            return FenValidationResult.Invalid("FEN string is null or empty");
+        else if (!HasOnlyValidFenCharacters(fen))
             return FenValidationResult.Invalid("FEN string contains invalid characters.");
         else if (HasConsecutiveWhitespaces(fen))
             return FenValidationResult.Invalid("FEN string contains consecutive spaces.");
-        else if (!HasValidNumberOfParts(fen))
-            return FenValidationResult.Invalid("FEN string contains invalid number of parts.");
+        else if (!HasValidNumberOfSpaces(fen))
+            return FenValidationResult.Invalid("FEN string contains invalid number of spaces.");
 
         return FenValidationResult.Valid();
 
     }
 
     public static bool HasOnlyValidFenCharacters(string fen) =>
-        fen.All(c => ValidFenCharacters.Contains(c));
+        fen.All(c => _validFenCharacters.Contains(c));
 
     public static bool HasConsecutiveWhitespaces(string fen)
     {
-        Regex regex = new Regex(@"\s{2,}");
-        return regex.IsMatch(fen);
+        return _consecutiveSpaces.IsMatch(fen);
     }
 
-    public static bool HasValidNumberOfParts(string fen) =>
+    public static bool HasValidNumberOfSpaces(string fen) =>
         fen.Count(c => c == ' ') == 5;
 }
