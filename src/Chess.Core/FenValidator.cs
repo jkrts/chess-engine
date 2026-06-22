ee6212347250ce89b5f5629b8d062d5670af0d84
jkrts <jkroberts2099@gmail.com>
2026-06-21 20:29:21 -0500

    added fen structure validation and unti testing

--- src/Chess.Core/FenValidator.cs
+++ src/Chess.Core/FenValidator.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 
 namespace Chess.Core;
@@ -15,108 +17,141 @@ public static class FenValidator
         '9'
     */
 
-    // TODO
-    // Break validation down into each part
-    // 1. Move List
-    // 2. Active Color
-    // 3. Castling Availability
-    // 4. En Passant Target
-    // 5. HalfMove clock
-    // 6. Fullmove number
-    // 7. Chess Logic for valid position????
-
-
+    private static readonly string _validFenCharacters = "0123456789pnbrqkPNBRQK/-w ";
+    private static readonly Regex _consecutiveWhitepaces = new Regex(@"\s{2,}", RegexOptions.Compiled);
     private static readonly string _validPiecePlacementChars = "12345678pnbrqkPNBRQK/";
-    private static readonly Regex _consecutiveSpaces = new Regex(@"\s{2,}");
-    private static readonly string _validFenCharacters = "012345678pnbrqkPNBRQK/-w ";
-
+    
     public static FenValidationResult Validate(string fen)
     {
-
-        if (string.IsNullOrWhiteSpace(fen))
-            return FenValidationResult.Invalid("FEN string is null or empty");
-        else if (!HasZeroConsecutiveWhitespaces(fen))
-            return FenValidationResult.Invalid("FEN string contains consecutive spaces.");
-        else if (!HasValidNumberOfSpaces(fen))
-            return FenValidationResult.Invalid("FEN string contains invalid number of spaces.");
-
-
-        var fenParts = fen.Split(' ');
-
-        if(!HasValidPiecePlacement(fenParts[0]))
-            return FenValidationResult.Invalid("Invalid Piece Placement in FEN string"); 
+        if (string.IsNullOrEmpty(fen))
+            return FenValidationResult.Invalid("Null Input.");
 
         if (!HasOnlyValidFenCharacters(fen))
             return FenValidationResult.Invalid("FEN string contains invalid characters.");
-        else if (HasConsecutiveWhitespaces(fen))
-            return FenValidationResult.Invalid("FEN string contains consecutive spaces.");
-        else if (!HasValidNumberOfSpaces(fen))
-            return FenValidationResult.Invalid("FEN string contains invalid number of spaces.");
 
+        if (!HasNoConsecutiveWhitespaces(fen))
+            return FenValidationResult.Invalid("FEN string contains consecutive whitespaces.");
+        
+        if (!HasNoLeadingTrailingWhitespace(fen))
+            return FenValidationResult.Invalid("FEN string begins or ends with a whitespace.");
 
-        if(!HasValidPiecePlacement(fenParts[0]))
-            return FenValidationResult.Invalid("Invalid Piece Placement in FEN string"); 
+        if (!HasValidNumberOfSpaces(fen))
+            return FenValidationResult.Invalid("FEN string has invalid number of spaces.");
+        
+        var fenParts = fen.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
+        if (!HasDataForEachPart(fenParts))
+            return FenValidationResult.Invalid("FEN string part count is invalid.");
+
+        // Validate part 1. Piece placement characters
+        if (!HasValidPiecePlacementChars(fenParts[0]))
+            return FenValidationResult.Invalid("FEN string has invalid characters in piece placement section.");
+        
+        // Validate part 2. Active color
+        if (!HasValidActiveColorChar(fenParts[1]))
+            return FenValidationResult.Invalid("FEN string has invalid active color.");
+
+        // Validate part 3. Castling availability
+        if (!HasValidCastlingAvailabilityChars(fenParts[2]))
+            return FenValidationResult.Invalid("FEN string has invalid castling availability.");
+
+        // Validate part 4. En passant target square
+        if (!HasValidEnPassantTargetSquareChars(fenParts[3]))
+            return FenValidationResult.Invalid("FEN string has invalid en passant target square.");
+
+        // Validate part 5. Halfmove clock
+        if (!HasValidHalfMoveClockChars(fenParts[4]))
+            return FenValidationResult.Invalid("FEN string has invalid halfmove clock.");
+        
+        // Validate part 6. Fullmove number
+        if (!HasValidFullMoveNumberChars(fenParts[5]))
+            return FenValidationResult.Invalid("FEN string has invalid fullmove number.");
 
         return FenValidationResult.Valid();
 
     }
 
-    public static bool HasZeroConsecutiveWhitespaces(string fen) =>
-        _consecutiveSpaces.IsMatch(fen) ? false : true;
-
-    public static bool HasValidNumberOfSpaces(string fen) =>
-        fen.Count(c => c == ' ') == 5;
-
-
-    public static bool HasValidPiecePlacement(string piecePlacement)
+    public static bool HasOnlyValidFenCharacters(string fen)
     {
-        // TODO
+        return fen.All(c => _validFenCharacters.Contains(c));
+    }
 
-        if(!piecePlacement.All(c => _validPiecePlacementChars.Contains(c)))
+    public static bool HasNoConsecutiveWhitespaces(string fen)
+    {
+        if (_consecutiveWhitepaces.IsMatch(fen))
             return false;
-
-
-
         return true;
     }
 
-    public static bool HasValidColor(string fen)
+    public static bool HasNoLeadingTrailingWhitespace(string fen)
     {
-        // TODO
+        if (char.IsWhiteSpace(fen[0]) || char.IsWhiteSpace(fen[fen.Length - 1]))
+            return false;
         return true;
     }
 
-    public static bool HasValidCastling(string fen)
+    public static bool HasValidNumberOfSpaces(string fen)
     {
-        // TODO
+        if (fen.Count(c => c == ' ') != 5)
+            return false;
         return true;
     }
 
-    public static bool HasValidEnPassantTarget(string fen)
+    public static bool HasDataForEachPart(string[] fenParts)
     {
-        // TODO
+        if (fenParts.Length != 6)
+            return false;
         return true;
     }
 
-    public static bool HasValidHalfmoveClock(string fen)
+    public static bool HasValidPiecePlacementChars(string fenPartPiecePlacement)
     {
-        // TODO
+        return fenPartPiecePlacement.All(c => _validPiecePlacementChars.Contains(c));
+    }
+
+    public static bool HasValidActiveColorChar(string fenPartActiveColor)
+    {
+        if (fenPartActiveColor.Length != 1)
+            return false;
+        if (fenPartActiveColor[0] != 'w' && fenPartActiveColor[0] != 'b')
+            return false;
         return true;
     }
 
-    public static bool HasValidFullmoveNumber(string fen)
+    public static bool HasValidCastlingAvailabilityChars(string fenPartCastlingAvailability)
     {
-        // TODO
-        return true;
+        if (fenPartCastlingAvailability.Length < 1 || fenPartCastlingAvailability.Length > 4)
+            return false;
+        if (fenPartCastlingAvailability == "-")
+            return true;
+        return fenPartCastlingAvailability.All(c => c is 'k' or 'K' or 'q' or 'Q');
     }
-    
-    public static bool HasOnlyValidFenCharacters(string fen) =>
-        fen.All(c => _validFenCharacters.Contains(c));
 
-    public static bool HasConsecutiveWhitespaces(string fen)
+    public static bool HasValidEnPassantTargetSquareChars(string fenPartEnPassant)
     {
-        // TODO
-        return true;
+        if (fenPartEnPassant == "-")
+            return true;
+        if (fenPartEnPassant.Length != 2)
+            return false;
+        
+        char file = fenPartEnPassant[0];
+        char rank = fenPartEnPassant[1];
+
+        return file is >= 'a' and <= 'h' && rank is '3' or '6';
     }
+
+    public static bool HasValidHalfMoveClockChars(string fenPartHalfMove)
+    {
+        if (int.TryParse(fenPartHalfMove, out int number) && number >= 0)
+            return true;
+        return false;
+    }
+
+    public static bool HasValidFullMoveNumberChars(string fenPartFullMove)
+    {
+        if (int.TryParse(fenPartFullMove, out int number) && number > 0)
+            return true;
+        return false;
+    }
+
 }
